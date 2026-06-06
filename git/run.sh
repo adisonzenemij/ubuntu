36a072e71e8417ccc63b43d807cdd6527cc1363f
Adison Jimenez <adisonzenemij@gmail.com>
2026-06-05 19:07:44 -0500

    feat: 8355a4a1-4a10-47d6-80f6-518f0e3e1bce

--- git/run.sh
+++ git/run.sh
@@ -40,10 +40,13 @@ ask_yes_no() {
 
   while true; do
     read -r -p "$prompt (S/N - Y/N): " answer
+    answer="${answer//$'\r'/}"
+    answer="${answer#"${answer%%[![:space:]]*}"}"
+    answer="${answer%"${answer##*[![:space:]]}"}"
     case "${answer,,}" in
       s|si|sí|y|yes) return 0 ;;
       n|no) return 1 ;;
-      *) echo "Respuesta no válida. Escribe S/N o Y/N." ;;
+      *) echo "Respuesta no válida. Escribe S, N, Y o Yes/No." ;;
     esac
   done
 }
